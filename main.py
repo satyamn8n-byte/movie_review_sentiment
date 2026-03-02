8ff784a9f0eb8d657af2f60c4b99345934fabdb6
Satyam74 <shatyamsharma7416@gmail.com>
2026-03-03 00:41:31 +0530

    ready_to_use

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
-# import os
-# import joblib
-# import re
-# from model.preprocessing import clean_tokenize
-# import nltk
-# from nltk.tokenize import word_tokenize
-# from nltk.stem import WordNetLemmatizer
-# nltk.download('wordnet')
-# nltk.download('punkt_tab')
-# from fastapi.templating import Jinja2Templates
-# from fastapi import FastAPI, Request, Form
-# from fastapi.responses import HTMLResponse
+import os
+import joblib
+import re
+from model.preprocessing import clean_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download('wordnet')
+nltk.download('punkt_tab')
+import time
+import streamlit as st
 
-# app = FastAPI()
-# templates = Jinja2Templates(directory="templates")
+# Load model
+@st.cache_resource
+def model_resources():
+    decoder = joblib.load(os.getcwd() + "/model/decoder.joblib")
+    model = joblib.load(os.getcwd() + "/model/NB_model.joblib")
+    return model, decoder
+
+model, decoder = model_resources()
+
+def infrence(text, model, decoder):
+        text = model.predict([text])
+        decodded = decoder.inverse_transform(text)
+        return decodded[0]
 
 
-# decoder = joblib.load(os.getcwd() + "/model/decoder.joblib")
-# model = joblib.load(os.getcwd() + "/model/NB_model.joblib")
+st.title("Movie Review Sentiment Analysis")
+
+    
+with st.form(key='review_form'):
+    user_review = st.text_area("Enter your Review:", key='review')
+    submit_button = st.form_submit_button("Send")
+
+if submit_button:
+    with st.spinner("Wait for it...", show_time=True):
+        time.sleep(2)
+    st.write(infrence(user_review, model, decoder))
 
 
 
-# @app.get('/predict')
-# def infrence(text:str):
 
-#     text = model.predict([text])
-#     decodded = decoder.inverse_transform(text)
-#     return decodded[0]
